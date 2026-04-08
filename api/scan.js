357551e3de2657a96ee519521a95718fbf5d8b79
sanjaynegi78 <sanjay.negi@shiprocket.com>
2026-04-08 18:07:55 +0530

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from '@supabase/supabase-js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
